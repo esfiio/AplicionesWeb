e1d163d38132a306986f57b1bf9efeeeef4dbdd5
esfiio <diegodanchez24@gmail.com>
2026-09-24 22:57:57 -0600

    [new] add fibonacci program

--- src/main/java/mx/edu/utez/proyecto1E/controller/FizzBuzzController.java
+++ src/main/java/mx/edu/utez/proyecto1E/controller/FizzBuzzController.java
@@ -21,6 +21,28 @@ public class FizzBuzzController {
                 System.out.println(i);
             }
         }
+
+        return "Diego Sanchez Carteno";
+    }
+
+    @GetMapping("/fibonacci/{n}")
+    public String fibonacci(@PathVariable int n ){
+        int anterior = 0;
+        int actual = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                System.out.println(anterior);
+            } else if (i == 1) {
+                System.out.println(actual);
+            } else {
+                int siguiente = anterior + actual;
+                System.out.println(siguiente);
+                anterior = actual;
+                actual = siguiente;
+            }
+        }
+
         return "Diego Sanchez Carteno";
     }
 }
